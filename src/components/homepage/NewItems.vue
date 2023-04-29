c5536c3b84ec6e9bdd172cea1ea877cf45b71447
ZechxCode <zechxwork@gmail.com>
2023-04-29 20:37:21 +0800

    Slicing Template to Component

---
+++ src/components/homepage/NewItems.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from 'vue'
+import NewItemsCard from '../NewItemsCard.vue';
+
+
+const NewItems = ref([
+    {
+        id: 1,
+        title: "Mobile UI Kit",
+        subcat: "Mobile UI Kit",
+        image: "items-1.jpg"
+    },
+    {
+        id: 2,
+        title: "Online Doctor Consultation",
+        subcat: "Website UI Kit",
+        image: "items-2.jpg"
+    },
+    {
+        id: 3,
+        title: "Banking Crypto",
+        subcat: "Mobile UI Kit",
+        image: "items-3.jpg"
+    },
+])
+</script>
+
+<template>
+    <div class="container px-4 mx-auto my-16 md:px-12">
+        <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">New Items</h2>
+        <div class="flex flex-wrap -mx-1 lg:-mx-4">
+            <NewItemsCard v-for="NewItem in NewItems" :key="NewItem.id" :title="NewItem.title" :subcat="NewItem.subcat"
+                :image="NewItem.image" />
+
+        </div>
+    </div>
+</template>
